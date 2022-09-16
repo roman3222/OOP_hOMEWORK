b5b7c9fb8021a06893b589408ec7b480ee74fb40
RomanKhopkin <roman.khopkin3222@gmail.com>
2022-09-16 20:24:49 +0900

    add method __lt__

--- main.py
+++ main.py
@@ -17,16 +17,24 @@ class Student:
         else:
             return 'Ошибка'
 
-    def counting_grades(self):
+    def __counting_grades(self):
         lst_grades = self.grades['Python']
         amount_lst = sum(lst_grades)
         average_rating = amount_lst / len(lst_grades)
-        average = round(average_rating, 2)
+        average = round(average_rating, 1)
         return average
+
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Not a Student')
+            return
+        return student_best.__counting_grades() > student_2.__counting_grades()
+
 
     def __str__(self):
         some_student = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашнее задание: ' \
-                       f'{student_best.counting_grades()}\n' \
+                       f'{student_best.__counting_grades()}\n' \
                        f'Курсы в процессе изучения: {",".join(self.courses_in_progress)}\nЗавершенные курсы: ' \
                        f'{" ".join(self.finished_courses)}'
         return some_student
@@ -45,15 +53,22 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.course_grades = {}
 
-    def calculation_grades(self):
+    def __calculation_grades(self):
         values = self.course_grades['Python']
         sum_lst = sum(values)
         midle_grade = sum_lst / len(values)
         return midle_grade
 
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Not a Lecturer')
+            return
+        return cool_lecturer.__calculation_grades() < lecturer_2.__calculation_grades()
+
     def __str__(self):
         self.some_lecturer = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: ' \
-                             f'{cool_lecturer.calculation_grades()}'
+                             f'{cool_lecturer.__calculation_grades()}'
         return self.some_lecturer
 
 
@@ -76,16 +91,28 @@ class Reviewer(Mentor):
         return self.some_reviever
 
 
-student_best = Student('John', 'Salivan', 'Famale')
+student_best = Student('John', 'Salivan', 'Male')
 student_best.courses_in_progress += ['Python', 'Git']
 student_best.finished_courses += ['Files']
+
+student_2 = Student('Linda', 'Mazovski', 'Female')
+student_2.courses_in_progress += ['Python', 'Git']
+student_2.finished_courses += ['Files']
 
 cool_lecturer = Lecturer('Mike', 'Vazovski')
 cool_lecturer.courses_attached += ['Python']
 
+lecturer_2 = Lecturer('Denzel', 'Washington')
+lecturer_2.courses_attached += ['Python']
+
 student_best.rate(cool_lecturer, 'Python', 10)
 student_best.rate(cool_lecturer, 'Python', 8)
 student_best.rate(cool_lecturer, 'Python', 9)
+
+student_2.rate(lecturer_2, 'Python', 8)
+student_2.rate(lecturer_2, 'Python', 7)
+student_2.rate(lecturer_2, 'Python', 5)
+
 
 some_reviewer = Reviewer('Jimi', 'Acha-Acha')
 some_reviewer.courses_attached += ['Python']
@@ -93,6 +120,12 @@ some_reviewer.courses_attached += ['Python']
 some_reviewer.rate_hw(student_best, 'Python', 7)
 some_reviewer.rate_hw(student_best, 'Python', 9)
 some_reviewer.rate_hw(student_best, 'Python', 10)
+
+some_reviewer.rate_hw(student_2, 'Python', 6)
+some_reviewer.rate_hw(student_2, 'Python', 9)
+some_reviewer.rate_hw(student_2, 'Python', 10)
+
+
 
 
 
@@ -103,3 +136,7 @@ print('Lecturer:')
 print(cool_lecturer.__str__())
 print('Reviewer:')
 print(some_reviewer.__str__())
+print(f'Средняя оценка   John Salivan больше чем у  Linda Mazovski ?: {student_best.__lt__(student_2)}')
+print(f'Средняя оценка у лектора Mike Vazovski меньше чем у Denzel Washington ?: {cool_lecturer.__lt__(lecturer_2)}')
+
+
